bd3e3adff1d8bd06a1de7525b644601213d1177d
Ladif <maxime.monceau@hotmail.fr>
2023-03-08 21:02:50 +0100

    slider

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, Inject, PLATFORM_ID, HostListener  } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, HostListener } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ItemService } from '../services/item.service';
 import Splide from '@splidejs/splide';
@@ -9,13 +9,13 @@ import { BehaviorSubject, Observable } from 'rxjs';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent implements AfterViewInit {
+export class ProductComponent  {
 
   infoItem: any;
   splide: any;
   image = []
-  constructor(@Inject(PLATFORM_ID) private platformId: Object, private route: ActivatedRoute, private item: ItemService){
-    
+  constructor(@Inject(PLATFORM_ID) private platformId: Object, private route: ActivatedRoute, private item: ItemService) {
+
     /*this.item.getItem().subscribe((data)=>{
       if(data){
         this.infoItem = data[this.route.snapshot.params['id']];
@@ -24,20 +24,24 @@ export class ProductComponent implements AfterViewInit {
       }
       
     })*/
-    this.item.get().subscribe((data: any)=>{
-      if(data){
+    this.item.get().subscribe((data: any) => {
+      if (data) {
         this.infoItem = data[this.route.snapshot.params['id']];
         this.image = this.infoItem.images;
+
+        const sleep = (m: number | undefined) => new Promise(r => setTimeout(r, m));
+
+        if (isPlatformBrowser(this.platformId)) {
+          (async () => {
+            await sleep(10);
+            this.splide = new Splide('#image-carousel');
+            this.splide.mount();
+          })()
+
+        }
+
       }
     })
   }
 
-  ngAfterViewInit() {
-    if (isPlatformBrowser(this.platformId)) {
-      this.splide = new Splide('#image-carousel');
-      this.splide.mount();
-    }
-    
-  }
-  
 }
